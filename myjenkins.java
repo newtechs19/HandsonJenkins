09b807b1e34f8dda0de6bb81ddb3bac6aa12994e
Praveen Thomas <53839225+newtechs19@users.noreply.github.com>
2019-08-28 18:41:52 +0530

    myjenkins

---
+++ myjenkins.java
@@ -0,0 +1,8 @@
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello my hands on Jenkins" to standard output.
+ */
+public class Handsonjenkins {
+  public static void main(String[] args) {
+    System.out.println("My, Hands, On, Jenkins");
+  }
+}
